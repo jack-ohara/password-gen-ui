c4d3a4c3c928ccee281180c918e1c2b7882869d6
Jack O'Hara <jack.ohara@protonmail.com>
2019-07-30 19:42:58 +0100

    Messing around. Don't really have anything in here. Probably going to abandon this branch

--- password-gen-ui/src/App.js
+++ password-gen-ui/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PasswordGenerator from "./Components/PasswordGenerator/PasswordGenerator";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <PasswordGenerator />
     </div>
   );
 }
---
+++ password-gen-ui/src/Components/PasswordGenerator/PasswordGenerator.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./PasswordGenerator.css";
+
+export default function PasswordGenerator() {
+  return (
+    <div>
+      <h1>Password Generator</h1>
+    </div>
+  );
+}
