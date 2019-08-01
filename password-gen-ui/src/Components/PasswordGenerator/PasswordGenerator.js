fdeb8e5ba458b838dba745488cdadfa0045df928
Jack O'Hara <jack.ohara@protonmail.com>
2019-08-01 06:53:10 +0100

    Created the basic layout for the site. A header and two sections in the main body. One for the options and one for the result

--- password-gen-ui/src/Components/PasswordGenerator/PasswordGenerator.js
+++ password-gen-ui/src/Components/PasswordGenerator/PasswordGenerator.js
@@ -3,8 +3,14 @@ import "./PasswordGenerator.css";
 
 export default function PasswordGenerator() {
   return (
-    <div>
-      <h1>Password Generator</h1>
+    <div className="password-generator">
+      <header>
+        <h1>PasswordGenerator</h1>
+      </header>
+      <div className="layout-container">
+        <div className="form-area">Your options...</div>
+        <div className="result-area">Your password...</div>
+      </div>
     </div>
   );
 }
